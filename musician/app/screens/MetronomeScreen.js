9033bfef0d44c72a7ed20dcf67a7166a364447b3
raramai-coder <1846409@students.wits.ac.za>
2022-04-02 13:16:17 +0200

    Put input text on metronome

--- musician/app/screens/MetronomeScreen.js
+++ musician/app/screens/MetronomeScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {SafeAreaView,TouchableHighlight,Image,StyleSheet, TouchableOpacity, Text, ImageBackground } from 'react-native';
+import {SafeAreaView,TouchableHighlight,Image,StyleSheet, TouchableOpacity, Text, ImageBackground, TextInput } from 'react-native';
 
-const metronomePress = ()=>console.log("MetronopmePressed")
+import colors from '../config/colors';
+
+
+
 
 function MetronomeScreen(props) {
+    
+    const metronomePress = ()=>console.log("MetronopmePressed");    //what gets called when the metronome is pressed
+    const [number, onChangeNumber] = React.useState(null);          // what gets called when the user inputs a number into the bpm text input
+
+    
+    
     return (
         <SafeAreaView>
             <TouchableOpacity onPress={metronomePress}>
                 <ImageBackground source={require("../../assets/metronome-image-wb.png")} resizeMode='contain' style={styles.metronomeImage} >
+                    <TextInput onChangeText={onChangeNumber}
+                        value ={number}
+                        placeholder='120'
+                        keyboardType='numeric'
+                        style={styles.bpmTextInput} ></TextInput>
                    
                 </ImageBackground>
 
@@ -21,6 +35,14 @@ const styles = StyleSheet.create({
     metronomeImage:{
         width:'100%',
         height:'100%',
+        alignItems:'center',
+    },
+    bpmTextInput:{
+        paddingTop:125,
+        //height:50,
+        fontSize:25,
+        color: colors.black,
+
     }
     
 })
