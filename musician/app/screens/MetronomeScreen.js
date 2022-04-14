1b106ebfe7e352f168297339d0996f88c3fb745c
Mila <milamhlana201@gmail.com>
2022-04-14 18:28:02 +0200

    updating chages from main into branch

--- musician/app/screens/MetronomeScreen.js
+++ musician/app/screens/MetronomeScreen.js
@@ -1,10 +1,10 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState } from 'react';  //libraries imported from external sources
 import { render } from 'react-dom';
-import { SafeAreaView, TouchableHighlight, Image, StyleSheet, TouchableOpacity, Text, ImageBackground, TextInput, Button, Modal,Picker} from 'react-native';
-import click1 from '../click1.mp3';
-import hardClick from '../hardClick.mp3'
+import { SafeAreaView, TouchableHighlight, Image, StyleSheet, TouchableOpacity, Text, ImageBackground, TextInput, Button, Modal, Picker } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import Slider from '@react-native-community/slider'
+
+import click1 from '../click1.mp3';      //objects and libraries imported from within our project
+import hardClick from '../hardClick.mp3'
 import colors from '../config/colors';
 
 export default class MetronomeScreen extends Component {
@@ -16,29 +16,31 @@ export default class MetronomeScreen extends Component {
   constructor() {
     super();
 
+    //initial state of the app and its componets/functions
     this.state = {
-      playing: false,
+      playing: false, // there is nothing being played yet
       count: 0,
-      bpm: 100,
-      beatsPerMeasure: 4,
-      tempoText: "Moderato (moderately)"
+      bpm: 100, // initial beats per minute value
+      beatsPerMeasure: 4, // initial beats per measure value 
+      tempoText: "Moderato (moderately)" // initial tempo marking based on the bpm value
 
     };
 
+    // initialise the audios we are going to use
     this.click1 = new Audio(click1);
     this.click2 = new Audio(hardClick);
   }
 
   handleBeatsPerMeasureChange = (e) => {
-    const beatsPerMeasure = e.target.value;
+    const beatsPerMeasure = e.target.value; // obtain value of the beats per measure from the dropdwon menu 
 
     this.setState({
-      beatsPerMeasure
+      beatsPerMeasure //set the value to the one obtained
     })
   }
 
   handleBpmChange = (e) => {
-    const bpm = e.target.value;
+    const bpm = e.target.value; //obtain value of the beats per measure from the user input
     if (this.state.playing) {
       clearInterval(this.timer);  //start a new timer
       this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
@@ -52,6 +54,7 @@ export default class MetronomeScreen extends Component {
       this.setState({ bpm });
     }
 
+    //#region If statement to select and sisplay the tempo marking based on the bpm value
     if (bpm < 20) {
       this.setState({ tempoText: "Larghissimo (very, very slow)" });
     } else if (bpm >= 20 && bpm < 40) {
@@ -73,8 +76,9 @@ export default class MetronomeScreen extends Component {
     } else if (bpm >= 180 && bpm < 200) {
       this.setState({ tempoText: "Presto (really fast)" });
     } else if (bpm > 200) {
-      this.setState({ tempoText: "Prestissimo (that's reeeeeally fast dude!)" });
+      this.setState({ tempoText: "Prestissimo (that's reeeally fast dude!)" });
     }
+    //#endregion
   };
 
   handleSubmit = (e) => {
@@ -88,7 +92,8 @@ export default class MetronomeScreen extends Component {
       this.setState({
         playing: false
       });
-    
+
+
     } else {
 
       //start again with the current bpm
@@ -96,10 +101,12 @@ export default class MetronomeScreen extends Component {
       this.setState(
         {
           count: 0,
-          playing: true
+          playing: true,
+
         },
         this.playClick
       );
+
     }
   };
 
@@ -116,71 +123,72 @@ export default class MetronomeScreen extends Component {
   };
 
 
-
   render() {
 
+    // initialise the variables we are going to use to be in the current state
     const { playing, bpm, beatsPerMeasure } = this.state;
     return (
-
       <SafeAreaView style={styles.container}>
-
-          <TouchableOpacity>
+        <Text style={styles.bpmText}>ENTER BEATS PER MINUTES:
+        </Text>  
+        <TouchableOpacity>
+          <TextInput style={styles.bpmTextInput} onChange={this.handleBpmChange} //text to indicate that the user should enter a bpm and a bpm text input
+            value={bpm} ></TextInput>
           <form>
-            <Picker color='red'
-          onChange={this.handleBeatsPerMeasureChange}     //bpm text input
-          value={beatsPerMeasure}>
-            <Picker.Item label = "bpm value" value = "0"></Picker.Item>
-            <Picker.Item label="2" value="2"></Picker.Item>
-            <Picker.Item label="3" value="3"></Picker.Item>
-            <Picker.Item label="4" value="4"></Picker.Item>
-            <Picker.Item label="5" value="5"></Picker.Item>
-            <Picker.Item label="6" value="6"></Picker.Item>
-            <Picker.Item label="7" value="7"></Picker.Item>
-            <Picker.Item label="8" value="8"></Picker.Item>
-            <Picker.Item label="9" value="9"></Picker.Item>
-            <Picker.Item label="10" value="10"></Picker.Item>
-            <Picker.Item label="11" value="11"></Picker.Item>
-            <Picker.Item label="12" value="12"></Picker.Item>
+            <Picker style={styles.pickerMenu}
+              onChange={this.handleBeatsPerMeasureChange}     //beats per measure dropdown menu, and function to be called when a value is picked
+              value={beatsPerMeasure}>
+              <Picker.Item label="beats per measure" value="0"></Picker.Item>
+              <Picker.Item label="2" value="2"></Picker.Item>
+              <Picker.Item label="3" value="3"></Picker.Item>
+              <Picker.Item label="4" value="4"></Picker.Item>
+              <Picker.Item label="5" value="5"></Picker.Item>
+              <Picker.Item label="6" value="6"></Picker.Item>
+              <Picker.Item label="7" value="7"></Picker.Item>
+              <Picker.Item label="8" value="8"></Picker.Item>
+              <Picker.Item label="9" value="9"></Picker.Item>
+              <Picker.Item label="10" value="10"></Picker.Item>
+              <Picker.Item label="11" value="11"></Picker.Item>
+              <Picker.Item label="12" value="12"></Picker.Item>
             </Picker>
-          <Picker>
-            <Picker.Item label="note value" value="0"></Picker.Item>
-          <Picker.Item label="semibreve" value="1"></Picker.Item>
-          <Picker.Item label="semibreve rest" value="1"></Picker.Item>
-          <Picker.Item label="minim" value="2"></Picker.Item>
-          <Picker.Item label="minim rest" value="2"></Picker.Item>
-          <Picker.Item label="crochet" value="4"></Picker.Item>
-          <Picker.Item label="crochet rest" value="4"></Picker.Item>
-          <Picker.Item label="quaver" value="8"></Picker.Item>
-          <Picker.Item label="quaver rest" value="8"></Picker.Item>
-          <Picker.Item label="semiquaver" value="16"></Picker.Item>
-          <Picker.Item label="semiquaver rest" value="16"></Picker.Item>
-          </Picker>
+            <Picker style={styles.pickerMenu} //drop down menu to select the note per measure
+            > 
+              <Picker.Item label="note per measure" value="0"></Picker.Item> 
+              <Picker.Item label="1" value="1"></Picker.Item>
+              <Picker.Item label="2" value="2"></Picker.Item>
+              <Picker.Item label="3" value="3"></Picker.Item>
+              <Picker.Item label="4" value="4"></Picker.Item>
+              <Picker.Item label="5" value="5"></Picker.Item>
+              <Picker.Item label="6" value="6"></Picker.Item>
+              <Picker.Item label="7" value="7"></Picker.Item>
+              <Picker.Item label="8" value="8"></Picker.Item>
+              <Picker.Item label="9" value="9"></Picker.Item>
+              <Picker.Item label="10" value="10"></Picker.Item>
+              <Picker.Item label="11" value="11"></Picker.Item>
+              <Picker.Item label="12" value="12"></Picker.Item>
+            </Picker>
           </form>
-           <form>
-             <input onChange={this.handleBpmChange}     //bpm text input
-                value={bpm}
-       
-              >
-              </input >
-            </form >   
-                    
-           </TouchableOpacity>
-          
-          <ImageBackground source={require("../../assets/metronome-image-wb.png")} resizeMode='contain' style={styles.metronomeImage} >
-           
-          
-            <Text style={styles.timeSignatureText}>4/4</Text>
-            <Text
-              style={styles.speedText}>
-              {this.state.tempoText}
-            </Text>
-            <TouchableOpacity>
-               <Button title='Start/Stop' color='blue' onPress={this.startStop}/>
-            </TouchableOpacity>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.playButton} onPress={this.startStop} // a button to start or stop the metronome sound
+        >
+          <Text style={styles.buttonText}>START/STOP</Text>
+        </TouchableOpacity>
+        <ImageBackground source={require("../../assets/metronome-image-wb.png")} resizeMode='contain' style={styles.metronomeImage} >
 
-          </ImageBackground >
-         
-    </SafeAreaView>
+          <Text
+          //time signature text
+            style={styles.timeSignatureText}>
+            {this.state.beatsPerMeasure} / {this.state.notesPerMeasure}</Text>
+
+          <Text
+          //bpm text
+            style={styles.speedText}>
+            {this.state.tempoText}
+          </Text>
+        </ImageBackground>
+
+
+      </SafeAreaView>
 
 
     );
@@ -190,45 +198,85 @@ export default class MetronomeScreen extends Component {
 
 
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({    //styles for elements listed in Alphabetical order (with the exception of container - which is always on top)
   container: {
     flex: 1,
     justifyContent: "center",
-    paddingHorizontal: 20
+    alignContent: 'center',
+    //backgroundColor:colors.black,
   },
-  button: {
-    borderRadius: 8,
-    borderRadius:100,
-    paddingHorizontal: 14,
+  bpmText: {
+    paddingTop: 150,
+    color: colors.black,
+    fontSize: 15,
+    paddingLeft: '42.5%',
     justifyContent: 'center',
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    paddingVertical: 10
-    
+    alignContent: 'center'
   },
   bpmTextInput: {
-    paddingTop: 125,
-    //height:50,
-    fontSize: 25,
+    borderWidth: 1,
+    borderColor: '#777',
+    paddingTop: 5,
+    marginLeft:'42.5%',
+    marginTop: 10,
+    width: 200,
+    fontSize: 15,
+    color: colors.black,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: colors.userInputElement,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    paddingLeft: 20,
     color: colors.black,
 
   },
-   metronomeImage: {
+  metronomeImage: {
+    // margin:10,
     width: '100%',
-    height: '85%',
-  alignItems: 'center',
+    height: '90%',
+    alignItems: 'center',
+  },
+  pickerMenu: {
+    borderWidth: 1,
+    borderColor: '#777',
+    paddingTop: 5,
+    marginLeft: '42.5%',
+    marginTop: 10,
+    width: 200,
+    fontSize: 15,
+    color: colors.black,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#d4f3ee'
+  },
+  playButton: {
+    backgroundColor: colors.pressableElement,
+    height: 50,
+    width: '10%',
+    borderRadius: 10,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
   },
   speedText: {
-    paddingTop: 37.5,
+    paddingTop: 50,
+    paddingLeft: 90,
     color: colors.black,
-    fontSize: 20,
-    alignContent:'center'
+    fontSize: 17,
+    justifyContent: 'center',
+    alignContent: 'center',
+    //  backgroundColor: '#347572'
   },
+
   timeSignatureText: {
-    paddingTop: 335,
-    color: '#525252',
+    paddingTop: 550,
+    color: '#ffffff',
     fontSize: 20,
-  }
-
-
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
 });
