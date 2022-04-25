94e65f70de989f70d93334ba28b8481822fad38c
CiaranOtter <62837202+CiaranOtter@users.noreply.github.com>
2022-04-25 19:08:34 +0200

    commiting app.js staged changes

--- musician/App.js
+++ musician/App.js
@@ -1,10 +1,46 @@
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import MetronomeScreen from './app/screens/MetronomeScreen';
+import TunerScreen from './app/screens/TunerScreen';
+import ToolScreen from './app/screens/ToolScreen';
+import PracticeScreen from './app/screens/PracticeScreen';
+import SettingsScreen from './app/screens/SettingsScreen';
+
+import { Icon } from 'react-native-elements';
+
+import Navigator from './app/components/navigate';
+
+// const nav = useNavigationContainerRef();
 
 export default function App() {
-  return <MetronomeScreen></MetronomeScreen>
+  const nav = useNavigationContainerRef();
+  const Stack = new createStackNavigator();
+
+  return (
+    <View style={{ flex: 1 }}>
+      <NavigationContainer ref={nav} >
+                <Stack.Navigator >
+                    <Stack.Screen name="metronome" component={MetronomeScreen} />
+                    <Stack.Screen name="tuner" component={TunerScreen} />
+                    <Stack.Screen name="tools" component={ToolScreen} />
+                    <Stack.Screen name="practice" component={PracticeScreen} />
+                    <Stack.Screen name="settings" component={SettingsScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+
+      <View style={styles.nav_container}>
+        <Icon name="audiotrack" onPress={() => {nav.navigate('tuner')}}>Test</Icon>
+        <Icon name="details" onPress={() => {nav.navigate('metronome')}}>Test</Icon>
+        <Icon name="construction" onPress={() => {nav.navigate('tools')}}>Test</Icon>
+        <Icon name="content-paste" onPress={() => {nav.navigate('practice')}}>Test</Icon>
+        <Icon name="settings" onPress={() => {nav.navigate('settings')}}>Test</Icon>
+      </View>
+    </View>
+      // {/* <Button onPress={() => {nav.navigate("tuner")} }><Text>Test</Text></Button> */}
+  )
 }
 
 const styles = StyleSheet.create({
@@ -14,4 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  nav_container: {
+    flexDirection: 'row',
+    margin: "auto"
+  }
 });
