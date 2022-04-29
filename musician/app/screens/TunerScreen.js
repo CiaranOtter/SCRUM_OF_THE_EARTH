0253bf70e8797b7de1b503dcfb7db07f555ba230
raramai-coder <1846409@students.wits.ac.za>
2022-04-29 19:12:28 +0200

    record sound with button press

--- musician/app/screens/TunerScreen.js
+++ musician/app/screens/TunerScreen.js
@@ -1,29 +1,58 @@
-import React, { Component } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/stack';
-import { Button, View, Text } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/stack";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { Audio } from "expo-av";
+
 import colors from "../config/colors";
-import { StyleSheet } from "react-native";
 
+export default function TunerScreen() {
+  const [recording, setRecording] = React.useState();
 
-export default class TunerScreen extends Component {
+  async function startRecording() {
+    try {
+      console.log("Requesting Permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
 
-        constructor(){
-            super();
-        }
-        render() {
-            return (
-                <View style={ styles.container }>
-                    <Text>Screen for tuner</Text>
-                </View>
-            )    
-        }
-    
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (error) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Screen for tuner</Text>
+      <Button
+        title={recording ? "Stop Recording" : "Start Recording"}
+        onPress={recording ? stopRecording : startRecording}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({    //styles for elements listed in Alphabetical order (with the exception of container - which is always on top)
-    container: {
-      flex: 1,
-      backgroundColor:colors.white,
-    }
+const styles = StyleSheet.create({
+  //styles for elements listed in Alphabetical order (with the exception of container - which is always on top)
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
 });
