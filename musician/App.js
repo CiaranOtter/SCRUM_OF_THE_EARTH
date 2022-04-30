476c27bf0e7a2cff1985d5d7e49c632724422eb2
Ciaran <ciaranjotter@gmail.com>
2022-04-30 15:18:44 +0200

    Fixes made to App.js

--- musician/App.js
+++ musician/App.js
@@ -1,93 +1,84 @@
-// import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { StatusBar } from 'expo-status-bar';
-// import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
 
-// import MetronomeScreen from './app/screens/MetronomeScreen';
-// import TunerScreen from './app/screens/TunerScreen';
-// import ToolScreen from './app/screens/ToolScreen';
-// import PracticeScreen from './app/screens/PracticeScreen';
-// import SettingsScreen from './app/screens/SettingsScreen';
+import MetronomeScreen from './app/screens/MetronomeScreen';
+import TunerScreen from './app/screens/TunerScreen';
+import ToolScreen from './app/screens/ToolScreen';
+import PracticeScreen from './app/screens/PracticeScreen';
+import SettingsScreen from './app/screens/SettingsScreen';
 
-// import { Icon } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 
-// import Navigator from './app/components/navigate';
+import Navigator from './app/components/navigate';
 
-// // const nav = useNavigationContainerRef();
-
-// export default function App() {
-//   const nav = useNavigationContainerRef();
-//   const Stack = new createStackNavigator();
-
-//   return (
-//     <View style={{ flex: 1 }}>
-//       <NavigationContainer ref={nav} >
-//                 <Stack.Navigator >
-//                     <Stack.Screen name="metronome" component={MetronomeScreen} />
-//                     <Stack.Screen name="tuner" component={TunerScreen} />
-//                     <Stack.Screen name="tools" component={ToolScreen} />
-//                     <Stack.Screen name="practice" component={PracticeScreen} />
-//                     <Stack.Screen name="settings" component={SettingsScreen} />
-//                 </Stack.Navigator>
-//       </NavigationContainer>
-
-//       <View style={styles.nav_container}>
-//         <Pressable onPress={() => {nav.navigate('tuner')}} style={styles.icon_container}>
-//           <Icon name="audiotrack" />
-//           <Text>Tuner</Text>
-//         </Pressable>
-        
-//         <Pressable onPress={() => {nav.navigate('metronome')}} style={styles.icon_container}>
-//           <Icon name="details" />
-//           <Text>Metronome</Text>
-//         </Pressable>
-        
-//         <Pressable onPress={() => {nav.navigate('tools')}} style={styles.icon_container}>
-//           <Icon name="construction" />
-//           <Text>Tools</Text>
-//         </Pressable>
-        
-//         <Pressable onPress={() => {nav.navigate('practice')}} style={styles.icon_container}>
-//           <Icon name="content-paste" />
-//           <Text>Practice</Text>
-//         </Pressable>
-        
-//         <Pressable onPress={() => {nav.navigate('settings')}} style={styles.icon_container}>
-//           <Icon name="settings" />
-//           <Text>Settings</Text>
-//         </Pressable>
-        
-//       </View>
-//     </View>
-//   )
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   nav_container: {
-//     flexDirection: 'row',
-//     alignItems:'center',
-//     justifyContent: 'center'
-//   },
-
-//   icon_container: {
-//     margin: "auto",
-//     padding: 10,
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   }
-// });
-
-import Navigation from "./app/screens/Navigation";
+// const nav = useNavigationContainerRef();
 
 export default function App() {
+  const nav = useNavigationContainerRef();
+  const Stack = new createStackNavigator();
 
-  render(
-    <Navigation></Navigation>
+  return (
+    <View style={{ flex: 1 }}>
+      <NavigationContainer ref={nav} >
+                <Stack.Navigator >
+                    <Stack.Screen name="metronome" component={MetronomeScreen} />
+                    <Stack.Screen name="tuner" component={TunerScreen} />
+                    <Stack.Screen name="tools" component={ToolScreen} />
+                    <Stack.Screen name="practice" component={PracticeScreen} />
+                    <Stack.Screen name="settings" component={SettingsScreen} />
+                </Stack.Navigator>
+      </NavigationContainer>
+
+      <View style={styles.nav_container}>
+        <Pressable onPress={() => {nav.navigate('tuner')}} style={styles.icon_container}>
+          <Icon name="audiotrack" />
+          <Text>Tuner</Text>
+        </Pressable>
+        
+        <Pressable onPress={() => {nav.navigate('metronome')}} style={styles.icon_container}>
+          <Icon name="details" />
+          <Text>Metronome</Text>
+        </Pressable>
+        
+        <Pressable onPress={() => {nav.navigate('tools')}} style={styles.icon_container}>
+          <Icon name="construction" />
+          <Text>Tools</Text>
+        </Pressable>
+        
+        <Pressable onPress={() => {nav.navigate('practice')}} style={styles.icon_container}>
+          <Icon name="content-paste" />
+          <Text>Practice</Text>
+        </Pressable>
+        
+        <Pressable onPress={() => {nav.navigate('settings')}} style={styles.icon_container}>
+          <Icon name="settings" />
+          <Text>Settings</Text>
+        </Pressable>
+        
+      </View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nav_container: {
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'center'
+  },
+
+  icon_container: {
+    margin: "auto",
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
