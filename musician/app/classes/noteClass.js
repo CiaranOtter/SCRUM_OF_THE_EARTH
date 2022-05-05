fd790b14ead4fdbc9b7119629e9cffa9267e3b4e
Leslie Fabishilaki <leslie.fab85@gmail.com>
2022-05-05 11:58:03 +0200

    made some fixes to the pitch/note classifications class

--- musician/app/classes/noteClass.js
+++ musician/app/classes/noteClass.js
@@ -9,28 +9,24 @@ module.exports = class noteClass {
     // sets the name of the note
     setNoteName(n){
         this.noteName = n;
-        return this.noteName;
     }
 
     // set the lowest value in the range of the note frequency
     // this lowest value will be the starting point of the note being flat
     setPitchFlat(n){
         this.pitchFlat = n;
-        return this.pitchFlat;
     }
 
     // set the in-tune value of the note frequency
     // this is the value that the note is considered to be in tune
     setPitchTune(n){
         this.pitchTune = n;
-        return this.pitchTune;
     }
 
     // set the highest value in the range of the note frequency
     // this lowest value will be the ending point of the note being sharp
     setPitchSharp(n){
         this.pitchSharp = n;
-        return this.pitchSharp;
     }
 
     // the next set of functions returns the constructor values of this class
