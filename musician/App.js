21c82580a5f080322b5ea8efd6f7940f865ab248
Ciaran <ciaranjotter@gmail.com>
2022-05-18 10:31:04 +0200

    React pure ap into main branch

--- musician/App.js
+++ musician/App.js
@@ -1,83 +1,31 @@
-<<<<<<< HEAD
-import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'react-native';
-=======
 import React from 'react';
 import {
   NavigationContainer,
   useNavigationContainerRef,
 } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
->>>>>>> React-pure-backup
 import {Button, StyleSheet, Text, View, Pressable, Image} from 'react-native';
 import { mdiMusicCircle } from '@mdi/js';
 
-<<<<<<< HEAD
-import MetronomeScreen from './app/screens/MetronomeScreen';
-import _6SMTunerScreen from './app/screens/TunerScreen';
-import _4SMTunerScreen from './app/screens/4_StringManualTunerScreen';
-import _4SATunerScreen from './app/screens/4_StringAutomaticTunerScreen';
-=======
 import MetronomeScreen from './app/screens/MetronomeScreen.js';
 import _6SMTunerScreen from './app/screens/TunerScreen';
 import _4SMTunerScreen from './app/screens/4_StringManualTunerScreen';
 import _4SATunerScreen from './app/screens/4_StringAutomaticTunerScreen';
 import ChromaticScreen from './app/screens/ChromaticScreen';
->>>>>>> React-pure-backup
 import ToolScreen from './app/screens/ToolScreen';
 import PracticeScreen from './app/screens/PracticeScreen';
 import SettingsScreen from './app/screens/SettingsScreen';
 import BeatMakerScreen from './app/screens/BeatMakerScreen.js';
 
-<<<<<<< HEAD
-//manual tuner screen for testing purposes
-import ManualTunerScreen from './app/screens/ManualTunerScreen';
-
-import { Icon } from 'react-native-elements';
-
-import Navigator from './app/components/navigate';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import ChromaticScreen from './app/screens/ChromaticScreen';
-
-// const nav = useNavigationContainerRef();
-=======
 import {Icon} from 'react-native-elements';
 
 import {TouchableOpacity} from 'react-native-gesture-handler';
->>>>>>> React-pure-backup
 
 export default function App() {
   const nav = useNavigationContainerRef();
   const Stack = new createStackNavigator();
 
   return (
-<<<<<<< HEAD
-
-    <View style={{ flex: 1 }}>
-      <StatusBar
-        backgroundColor={"#fff"}
-        barStyle={"dark-content"}
-      />
-      <NavigationContainer ref={nav} >
-                <Stack.Navigator >
-                    <Stack.Screen options={{ headerShown: false }}  name="metronome" component={MetronomeScreen} />
-                    <Stack.Screen options={{ headerShown: false }} name="6SMTuner" component={_6SMTunerScreen} />
-                    <Stack.Screen options={{ headerShown: false }} name="4SMTuner" component={_4SMTunerScreen}/>
-                    <Stack.Screen options={{ headerShown: false }} name="4SATuner" component={_4SATunerScreen}/>
-                    <Stack.Screen options={{ headerShown: false }} name="Chromatic" component={ChromaticScreen}/>
-                    <Stack.Screen options={{ headerShown: false }}  name="tools" component={ToolScreen} />
-                    <Stack.Screen options={{ headerShown: false }}  name="practice" component={PracticeScreen} />
-                    <Stack.Screen options={{ headerShown: false }}  name="settings" component={SettingsScreen} />
-                </Stack.Navigator>
-      </NavigationContainer>
-
-      <View style={styles.nav_container}>
-        <Pressable onPress={() => {nav.navigate('4SMTuner')}} style={styles.icon_container}>
-          <Icon name="audiotrack" />
-          <Text>Tuner</Text>
-        </Pressable>
-=======
     <View style={{flex: 1}}>
       <NavigationContainer ref={nav}>
         <Stack.Navigator>
@@ -140,7 +88,6 @@ export default function App() {
             <Icon name="details" />
             <Text>Metronome</Text>
           </Pressable>
->>>>>>> React-pure-backup
         
           <Pressable onPress={() => {nav.navigate('tools')}} style={styles.icon_container}>
             <Icon name="construction" />
--- musician/app/screens/ChromaticScreen.js
+++ musician/app/screens/ChromaticScreen.js
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
-//import {ImageBackground, SafeAreaView, StyleSheet, TouchableOpacity, Alert, View } from "react-native";
-import SelectDropdown from "react-native-select-dropdown";
-import { StackActions } from "@react-navigation/native";
-
-
-const pitchNoteC = require("../classes/pitchNoteClassification.js");
-
-export default class ChromaticScreen extends Component{
-  
-  constructor(){
-    super();
-
-    this.state = {
-      tuner_type: ["4 String Tuner", "6 String Tuner", "Chromatic Tuner"]
-
-    }
-
-    this.pitchNoteClass = new pitchNoteC();
-    this.pitch = 5021;
-  }
-  
-  selectedTuner = (selectedItem) =>{
-    if(selectedItem == this.state.tuner_type[0]){
-        this.props.navigation.dispatch(StackActions.replace('4SMTuner'));
-        return this.state.tuner_type[0];
-    }
-    else if(selectedItem == this.state.tuner_type[1]){
-        this.props.navigation.dispatch(StackActions.replace('6SMTuner'));
-        return this.state.tuner_type[1]
-    }
-    else if(selectedItem == this.state.tuner_type[2]){
-        this.props.navigation.dispatch(StackActions.replace('Chromatic'));
-        return this.state.tuner_type[2]
-    }
-  }
-
-  render(){
-    const note = this.pitchNoteClass.getNote(this.pitch);
-    const classification = this.pitchNoteClass.getClassification(this.pitch);
-    const pitch = this.pitch;
-
-    return (
-
-      <SafeAreaView style={styles.container}>
-        <TouchableOpacity style={{marginTop:-100, marginLeft:-160}}>
-            <SelectDropdown 
-                data={this.state.tuner_type}
-                onSelect={() => {}}
-                buttonTextAfterSelection={(selectedItem, index) => {
-                return this.selectedTuner(selectedItem)
-                }}
-                rowTextForSelection={(item, index) => {
-                  return item
-                }}
-                defaultButtonText={this.state.tuner_type[2]}
-                buttonStyle={styles.DropDownStyle}
-                buttonTextStyle={styles.textStyle}
-                rowTextStyle={{fontSize: 15}}
-            />
-        </TouchableOpacity>
-        <Text style={styles.title}>{note}</Text>
-        <Text style={styles.frequency}>{pitch}Hz</Text>
-        <Text style={styles.indicator}>{classification}</Text>
-=======
 // making needed imports
 
 import React, {Component, useState} from 'react';
@@ -183,16 +116,12 @@ export default class ChromaticScreen extends Component {
         <Text style={styles.frequency}>{this.state.pitch}Hz</Text>
         {/* display the classification of the note (sharp, in-tune or flat) */}
         <Text style={styles.indicator}>{this.state.NoteClass}</Text>
->>>>>>> React-pure-backup
       </SafeAreaView>
     );
   }
 }
 
-<<<<<<< HEAD
-=======
 // styles for the chromatic tuner screen listed in alphabetical order
->>>>>>> React-pure-backup
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -201,29 +130,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   DropDownStyle: {
-<<<<<<< HEAD
-    width:150,
-    backgroundColor: '#55B7AD'
-  },
-  title: {
-    fontSize: 150,
-    color: 'gray',
-    marginTop: 100
-  },
-  indicator:{
-    fontSize: 24
-  },
-  frequency: {
-    fontSize: 30,
-    color: '#55B7AD'
-  },
-    textStyle:{
-      fontSize:15,
-        color:'white',
-        fontWeight:'bold',
-
-    },
-=======
     width: 150,
     backgroundColor: colors.userInputElement,
     height: 40,
@@ -245,5 +151,4 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: '#55B7AD',
   },
->>>>>>> React-pure-backup
 });
