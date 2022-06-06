e9c7426cd2c22fd53942938414690a4996ae60e6
CiaranOtter <62837202+CiaranOtter@users.noreply.github.com>
2022-06-06 12:18:05 +0200

    further updates

--- circleci-scripts/readme-setup.sh
+++ circleci-scripts/readme-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pwd
-if test -f "./README.md"; then
+if test -f "../README.md"; then
     echo "file exits"
     rm README.md
 fi
 
-merge ./readme-sources
-mv ./readme-sources/merged.md ./README.md
+merge ../readme-sources
+mv ../readme-sources/merged.md ../README.md
